13b2b54fbf0fd0498516a975434c2c3834f5f6b9
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2024-11-01 13:13:40 -0500

    feat: added more type of gases for the chip gas

--- gas.chip.c
+++ gas.chip.c
@@ -8,14 +8,24 @@
 typedef struct {
   pin_t pin_out;
   timer_t timer;
-  uint32_t gas_attr;
+  uint32_t gas_oxygen;
+  uint32_t gas_co2;
+  uint32_t gas_ethylene;
+  uint32_t gas_ammonia;
+  uint32_t gas_so2;
 } chip_state_t;
 
 // Function that updates the analog output
 static void update_output(void *ctx) {
   chip_state_t *chip = (chip_state_t *)ctx;
-  double gas_level = attr_read(chip->gas_attr);
-  float voltage = (gas_level / 4095.0) * 3.3;
+  double oxygen_level = attr_read(chip->gas_oxygen);
+  double co2_level = attr_read(chip->gas_co2);
+  double ethylene_level = attr_read(chip->gas_ethylene);
+  double ammonia_level = attr_read(chip->gas_ammonia);
+  double so2_level = attr_read(chip->gas_so2);
+
+  // Example: Calculate a combined voltage or handle each gas type separately
+  float voltage = ((oxygen_level + co2_level + ethylene_level + ammonia_level + so2_level) / (5 * 4095.0)) * 3.3;
   pin_dac_write(chip->pin_out, voltage);
 }
 
@@ -26,14 +36,18 @@ void chip_init() {
   pin_t pin_gnd = pin_init("GND", INPUT);
   chip->pin_out = pin_init("OUT", ANALOG);
 
-  // Create the attribute for the gas level
-  chip->gas_attr = attr_init("gas_level", 0.0);
+  // Create the attributes for each gas type
+  chip->gas_oxygen = attr_init("gas_oxygen", 0.0);
+  chip->gas_co2 = attr_init("gas_co2", 0.0);
+  chip->gas_ethylene = attr_init("gas_ethylene", 0.0);
+  chip->gas_ammonia = attr_init("gas_ammonia", 0.0);
+  chip->gas_so2 = attr_init("gas_so2", 0.0);
 
   // Configure the timer to update the output
   timer_config_t config = {
-      .callback = update_output,
-      .user_data = chip,
-  };
+    .callback = update_output,
+    .user_data = chip,
+};
   chip->timer = timer_init(&config);
 
   // Start timer every 100 ms
