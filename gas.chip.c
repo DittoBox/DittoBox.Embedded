c754aa85f7291c0b4d8c3034aa733cbf88004bf9
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2024-11-01 14:11:10 -0500

    feat: added pin modules and more reosurces for the chip gas

--- gas.chip.c
+++ gas.chip.c
@@ -1,12 +1,13 @@
-// gas.chip.c
-// SPDX-License-Identifier: MIT
-
 #include "wokwi-api.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct {
   pin_t pin_out;
+  pin_t pin_out2;
+  pin_t pin_out3;
+  pin_t pin_out4;
+  pin_t pin_out5;
   timer_t timer;
   uint32_t gas_oxygen;
   uint32_t gas_co2;
@@ -15,7 +16,6 @@ typedef struct {
   uint32_t gas_so2;
 } chip_state_t;
 
-// Function that updates the analog output
 static void update_output(void *ctx) {
   chip_state_t *chip = (chip_state_t *)ctx;
   double oxygen_level = attr_read(chip->gas_oxygen);
@@ -24,9 +24,11 @@ static void update_output(void *ctx) {
   double ammonia_level = attr_read(chip->gas_ammonia);
   double so2_level = attr_read(chip->gas_so2);
 
-  // Example: Calculate a combined voltage or handle each gas type separately
-  float voltage = ((oxygen_level + co2_level + ethylene_level + ammonia_level + so2_level) / (5 * 4095.0)) * 3.3;
-  pin_dac_write(chip->pin_out, voltage);
+  pin_dac_write(chip->pin_out, (oxygen_level / 4095.0) * 3.3);
+  pin_dac_write(chip->pin_out2, (co2_level / 4095.0) * 3.3);
+  pin_dac_write(chip->pin_out3, (ethylene_level / 4095.0) * 3.3);
+  pin_dac_write(chip->pin_out4, (ammonia_level / 4095.0) * 3.3);
+  pin_dac_write(chip->pin_out5, (so2_level / 4095.0) * 3.3);
 }
 
 void chip_init() {
@@ -35,22 +37,23 @@ void chip_init() {
   pin_t pin_vcc = pin_init("VCC", INPUT);
   pin_t pin_gnd = pin_init("GND", INPUT);
   chip->pin_out = pin_init("OUT", ANALOG);
+  chip->pin_out2 = pin_init("OUT2", ANALOG);
+  chip->pin_out3 = pin_init("OUT3", ANALOG);
+  chip->pin_out4 = pin_init("OUT4", ANALOG);
+  chip->pin_out5 = pin_init("OUT5", ANALOG);
 
-  // Create the attributes for each gas type
   chip->gas_oxygen = attr_init("gas_oxygen", 0.0);
   chip->gas_co2 = attr_init("gas_co2", 0.0);
   chip->gas_ethylene = attr_init("gas_ethylene", 0.0);
   chip->gas_ammonia = attr_init("gas_ammonia", 0.0);
   chip->gas_so2 = attr_init("gas_so2", 0.0);
 
-  // Configure the timer to update the output
   timer_config_t config = {
     .callback = update_output,
     .user_data = chip,
-};
+  };
   chip->timer = timer_init(&config);
 
-  // Start timer every 100 ms
   timer_start(chip->timer, 100000, true);
 
   printf("Gas sensor initialized.\n");
